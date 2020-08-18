09aa8f6d0f07fe4b36e79932e77fdb0295dc7752
lucas robert dos santos bueno <69163019+lucasrobert103@users.noreply.github.com>
2020-08-17 22:25:47 -0300

    Add files via upload

---
+++ Primeiro-Projeto_Generation/src/Alex/lucas.java
@@ -0,0 +1,89 @@
+/*
+ * To.
+ *
+ */
+package Alex;
+
+import java.util.Random;
+import javax.swing.JOptionPane;
+
+public class lucas {
+
+    public static void main(String[] args) {
+        String st, stNaipe, jop, totalCartas, cartaAtual;
+        String simbCarta;
+        int nValorTotal, valorCarta = 0, naipe = 0, simbolo = 0;
+        boolean denovo = true, perdeu = false;
+        JOptionPane.showMessageDialog(null, "Generation\nJogo 21\nClique para iniciar");
+        while(true){
+            Baralho b1 = new Baralho();
+            nValorTotal = 0;
+            totalCartas = "";
+            do{
+                perdeu = false;
+                valorCarta = 0;
+                Random random = new Random();
+                 do{
+                    naipe = random.nextInt(3);
+                    simbolo = random.nextInt(12);
+                }while(b1.verificaSeJaFoi(naipe, simbolo)== true);
+                simbCarta = b1.getCarta(naipe, simbolo);
+                b1.lancaCarta(naipe, simbolo);
+                valorCarta = b1.calculaValorCarta(simbolo);
+                stNaipe = b1.getNaipe(naipe, simbolo);
+                nValorTotal += b1.calculaValorCarta(simbolo);
+                cartaAtual = "|" + simbCarta + " " + stNaipe + "| ";
+                totalCartas = totalCartas + " " + cartaAtual;
+                jop = "Total Cartas: " + totalCartas + "\nValor total: " + nValorTotal;
+                if(nValorTotal > 21){
+                    JOptionPane.showMessageDialog(null, jop + "\nQue Pena Você Perdeu", "Fim de Jogo", JOptionPane.ERROR_MESSAGE);
+                    perdeu = true;
+                    break;
+                }
+                st = JOptionPane.showInputDialog(jop + "\n Quer Lançar outra carta? (S/N) ou 0 para sair");
+                if(st.equals("S") || st.equals("s"))
+                    denovo = true;
+                else if(st.equals("0"))
+                    return;
+                else
+                    denovo = false;
+            }while(denovo == true); 
+            //O JOGADOR DECIDIU PARAR
+            if(perdeu == false){
+                int scoreJogador = nValorTotal;
+                
+                
+                //VEZ DA MAQUINA JOGAR
+                
+                b1 = new Baralho();
+                do{
+                    valorCarta = 0;
+                    Random random = new Random();
+                    do{
+                        naipe = random.nextInt(3);
+                        if(nValorTotal <= 15)
+                            simbolo = random.nextInt(12);
+                        else
+                            simbolo = random.nextInt(4);
+                    }while(b1.verificaSeJaFoi(naipe, simbolo)== true);
+                    simbCarta = b1.getCarta(naipe, simbolo);
+                    b1.lancaCarta(naipe, simbolo);
+                    valorCarta = b1.calculaValorCarta(simbolo);
+                    stNaipe = b1.getNaipe(naipe, simbolo);
+                    nValorTotal += b1.calculaValorCarta(simbolo);
+                    cartaAtual = "|" + simbCarta + " " + stNaipe + "| ";
+                    totalCartas = totalCartas + " " + cartaAtual;                
+                }while(nValorTotal <= scoreJogador);
+                jop = "Cartas CPU: " + totalCartas;
+                jop = jop + "\nScore Jogador: "+ scoreJogador + "\nScore CPU: "
+                    + nValorTotal;
+                if(nValorTotal > 21){
+                    JOptionPane.showMessageDialog(null, jop + "\nVocêª Venceu! :D");
+                }else{
+                    JOptionPane.showMessageDialog(null, jop + "\nVocêª Perdeu D:");
+                }
+            }
+        }
+    }
+    
+}
